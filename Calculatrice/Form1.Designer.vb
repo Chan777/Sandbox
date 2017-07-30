d4838bcec5a87e6d6474bae9661844a1470bfc15
Chantal Morin <tchanboo@gmail.com>
2017-07-30 16:39:51 -0400

    Calculator

---
+++ Calculatrice/Form1.Designer.vb
@@ -0,0 +1,378 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form remplace la méthode Dispose pour nettoyer la liste des composants.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requise par le Concepteur Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
+    'Elle peut être modifiée à l'aide du Concepteur Windows Form.  
+    'Ne la modifiez pas à l'aide de l'éditeur de code.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtOperation = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Button9 = New System.Windows.Forms.Button()
+        Me.Button0 = New System.Windows.Forms.Button()
+        Me.ButtonPOINT = New System.Windows.Forms.Button()
+        Me.ButtonDIVI = New System.Windows.Forms.Button()
+        Me.ButtonMULT = New System.Windows.Forms.Button()
+        Me.ButtonADDI = New System.Windows.Forms.Button()
+        Me.ButtonSOUS = New System.Windows.Forms.Button()
+        Me.ButtonTOTAL = New System.Windows.Forms.Button()
+        Me.ButtonBACK = New System.Windows.Forms.Button()
+        Me.ButtonC = New System.Windows.Forms.Button()
+        Me.ButtonPLUSMOINS = New System.Windows.Forms.Button()
+        Me.txtTOTAL = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'txtOperation
+        '
+        Me.txtOperation.Font = New System.Drawing.Font("Arial", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtOperation.Location = New System.Drawing.Point(5, 4)
+        Me.txtOperation.MaxLength = 35
+        Me.txtOperation.Multiline = True
+        Me.txtOperation.Name = "txtOperation"
+        Me.txtOperation.Size = New System.Drawing.Size(234, 51)
+        Me.txtOperation.TabIndex = 0
+        Me.txtOperation.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.Button1.Font = New System.Drawing.Font("Arial", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.ForeColor = System.Drawing.Color.White
+        Me.Button1.Location = New System.Drawing.Point(5, 204)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(54, 43)
+        Me.Button1.TabIndex = 1
+        Me.Button1.TabStop = False
+        Me.Button1.Text = "1"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.Button2.Font = New System.Drawing.Font("Arial", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.ForeColor = System.Drawing.Color.White
+        Me.Button2.Location = New System.Drawing.Point(65, 204)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(54, 43)
+        Me.Button2.TabIndex = 2
+        Me.Button2.TabStop = False
+        Me.Button2.Text = "2"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Button3
+        '
+        Me.Button3.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.Button3.Font = New System.Drawing.Font("Arial", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button3.ForeColor = System.Drawing.Color.White
+        Me.Button3.Location = New System.Drawing.Point(125, 204)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(54, 43)
+        Me.Button3.TabIndex = 3
+        Me.Button3.TabStop = False
+        Me.Button3.Text = "3"
+        Me.Button3.UseVisualStyleBackColor = False
+        '
+        'Button4
+        '
+        Me.Button4.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.Button4.Font = New System.Drawing.Font("Arial", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button4.ForeColor = System.Drawing.Color.White
+        Me.Button4.Location = New System.Drawing.Point(5, 155)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(54, 43)
+        Me.Button4.TabIndex = 4
+        Me.Button4.TabStop = False
+        Me.Button4.Text = "4"
+        Me.Button4.UseVisualStyleBackColor = False
+        '
+        'Button5
+        '
+        Me.Button5.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.Button5.Font = New System.Drawing.Font("Arial", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button5.ForeColor = System.Drawing.Color.White
+        Me.Button5.Location = New System.Drawing.Point(65, 155)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(54, 43)
+        Me.Button5.TabIndex = 5
+        Me.Button5.TabStop = False
+        Me.Button5.Text = "5"
+        Me.Button5.UseVisualStyleBackColor = False
+        '
+        'Button6
+        '
+        Me.Button6.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.Button6.Font = New System.Drawing.Font("Arial", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button6.ForeColor = System.Drawing.Color.White
+        Me.Button6.Location = New System.Drawing.Point(125, 155)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(54, 43)
+        Me.Button6.TabIndex = 6
+        Me.Button6.TabStop = False
+        Me.Button6.Text = "6"
+        Me.Button6.UseVisualStyleBackColor = False
+        '
+        'Button7
+        '
+        Me.Button7.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.Button7.Font = New System.Drawing.Font("Arial", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button7.ForeColor = System.Drawing.Color.White
+        Me.Button7.Location = New System.Drawing.Point(5, 107)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(54, 43)
+        Me.Button7.TabIndex = 7
+        Me.Button7.TabStop = False
+        Me.Button7.Text = "7"
+        Me.Button7.UseVisualStyleBackColor = False
+        '
+        'Button8
+        '
+        Me.Button8.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.Button8.Font = New System.Drawing.Font("Arial", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button8.ForeColor = System.Drawing.Color.White
+        Me.Button8.Location = New System.Drawing.Point(65, 106)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(54, 43)
+        Me.Button8.TabIndex = 8
+        Me.Button8.TabStop = False
+        Me.Button8.Text = "8"
+        Me.Button8.UseVisualStyleBackColor = False
+        '
+        'Button9
+        '
+        Me.Button9.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.Button9.Font = New System.Drawing.Font("Arial", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button9.ForeColor = System.Drawing.Color.White
+        Me.Button9.Location = New System.Drawing.Point(125, 106)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Size = New System.Drawing.Size(54, 43)
+        Me.Button9.TabIndex = 9
+        Me.Button9.TabStop = False
+        Me.Button9.Text = "9"
+        Me.Button9.UseVisualStyleBackColor = False
+        '
+        'Button0
+        '
+        Me.Button0.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.Button0.Font = New System.Drawing.Font("Arial", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button0.ForeColor = System.Drawing.Color.White
+        Me.Button0.Location = New System.Drawing.Point(65, 253)
+        Me.Button0.Name = "Button0"
+        Me.Button0.Size = New System.Drawing.Size(54, 43)
+        Me.Button0.TabIndex = 10
+        Me.Button0.TabStop = False
+        Me.Button0.Text = "0"
+        Me.Button0.UseVisualStyleBackColor = False
+        '
+        'ButtonPOINT
+        '
+        Me.ButtonPOINT.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.ButtonPOINT.Font = New System.Drawing.Font("Arial", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ButtonPOINT.ForeColor = System.Drawing.Color.White
+        Me.ButtonPOINT.Location = New System.Drawing.Point(125, 253)
+        Me.ButtonPOINT.Name = "ButtonPOINT"
+        Me.ButtonPOINT.Size = New System.Drawing.Size(54, 43)
+        Me.ButtonPOINT.TabIndex = 12
+        Me.ButtonPOINT.TabStop = False
+        Me.ButtonPOINT.Text = "."
+        Me.ButtonPOINT.UseVisualStyleBackColor = False
+        '
+        'ButtonDIVI
+        '
+        Me.ButtonDIVI.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.ButtonDIVI.Font = New System.Drawing.Font("Symbol", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(2, Byte))
+        Me.ButtonDIVI.ForeColor = System.Drawing.Color.White
+        Me.ButtonDIVI.Location = New System.Drawing.Point(185, 107)
+        Me.ButtonDIVI.Name = "ButtonDIVI"
+        Me.ButtonDIVI.Size = New System.Drawing.Size(54, 43)
+        Me.ButtonDIVI.TabIndex = 13
+        Me.ButtonDIVI.TabStop = False
+        Me.ButtonDIVI.Text = "¸"
+        Me.ButtonDIVI.UseVisualStyleBackColor = False
+        '
+        'ButtonMULT
+        '
+        Me.ButtonMULT.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.ButtonMULT.Font = New System.Drawing.Font("Symbol", 20.25!)
+        Me.ButtonMULT.ForeColor = System.Drawing.Color.White
+        Me.ButtonMULT.Location = New System.Drawing.Point(185, 154)
+        Me.ButtonMULT.Name = "ButtonMULT"
+        Me.ButtonMULT.Size = New System.Drawing.Size(54, 43)
+        Me.ButtonMULT.TabIndex = 14
+        Me.ButtonMULT.TabStop = False
+        Me.ButtonMULT.Text = "´"
+        Me.ButtonMULT.UseVisualStyleBackColor = False
+        '
+        'ButtonADDI
+        '
+        Me.ButtonADDI.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.ButtonADDI.Font = New System.Drawing.Font("Symbol", 20.25!)
+        Me.ButtonADDI.ForeColor = System.Drawing.Color.White
+        Me.ButtonADDI.Location = New System.Drawing.Point(185, 203)
+        Me.ButtonADDI.Name = "ButtonADDI"
+        Me.ButtonADDI.Size = New System.Drawing.Size(54, 43)
+        Me.ButtonADDI.TabIndex = 15
+        Me.ButtonADDI.TabStop = False
+        Me.ButtonADDI.Text = "+"
+        Me.ButtonADDI.UseVisualStyleBackColor = False
+        '
+        'ButtonSOUS
+        '
+        Me.ButtonSOUS.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.ButtonSOUS.Font = New System.Drawing.Font("Symbol", 20.25!)
+        Me.ButtonSOUS.ForeColor = System.Drawing.Color.White
+        Me.ButtonSOUS.Location = New System.Drawing.Point(185, 252)
+        Me.ButtonSOUS.Name = "ButtonSOUS"
+        Me.ButtonSOUS.Size = New System.Drawing.Size(54, 43)
+        Me.ButtonSOUS.TabIndex = 16
+        Me.ButtonSOUS.TabStop = False
+        Me.ButtonSOUS.Text = "-"
+        Me.ButtonSOUS.UseVisualStyleBackColor = False
+        '
+        'ButtonTOTAL
+        '
+        Me.ButtonTOTAL.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.ButtonTOTAL.Font = New System.Drawing.Font("Symbol", 20.25!)
+        Me.ButtonTOTAL.ForeColor = System.Drawing.Color.White
+        Me.ButtonTOTAL.Location = New System.Drawing.Point(3, 299)
+        Me.ButtonTOTAL.Name = "ButtonTOTAL"
+        Me.ButtonTOTAL.Size = New System.Drawing.Size(236, 53)
+        Me.ButtonTOTAL.TabIndex = 17
+        Me.ButtonTOTAL.TabStop = False
+        Me.ButtonTOTAL.Text = "="
+        Me.ButtonTOTAL.UseVisualStyleBackColor = False
+        '
+        'ButtonBACK
+        '
+        Me.ButtonBACK.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.ButtonBACK.Font = New System.Drawing.Font("Symbol", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(2, Byte))
+        Me.ButtonBACK.ForeColor = System.Drawing.Color.White
+        Me.ButtonBACK.Location = New System.Drawing.Point(125, 59)
+        Me.ButtonBACK.Name = "ButtonBACK"
+        Me.ButtonBACK.Size = New System.Drawing.Size(114, 43)
+        Me.ButtonBACK.TabIndex = 18
+        Me.ButtonBACK.TabStop = False
+        Me.ButtonBACK.Text = "¬"
+        Me.ButtonBACK.UseVisualStyleBackColor = False
+        '
+        'ButtonC
+        '
+        Me.ButtonC.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.ButtonC.ForeColor = System.Drawing.Color.White
+        Me.ButtonC.Location = New System.Drawing.Point(5, 59)
+        Me.ButtonC.Name = "ButtonC"
+        Me.ButtonC.Size = New System.Drawing.Size(114, 43)
+        Me.ButtonC.TabIndex = 19
+        Me.ButtonC.TabStop = False
+        Me.ButtonC.Text = "C"
+        Me.ButtonC.UseVisualStyleBackColor = False
+        '
+        'ButtonPLUSMOINS
+        '
+        Me.ButtonPLUSMOINS.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.ButtonPLUSMOINS.Font = New System.Drawing.Font("Arial", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ButtonPLUSMOINS.ForeColor = System.Drawing.Color.White
+        Me.ButtonPLUSMOINS.Location = New System.Drawing.Point(5, 253)
+        Me.ButtonPLUSMOINS.Name = "ButtonPLUSMOINS"
+        Me.ButtonPLUSMOINS.Size = New System.Drawing.Size(54, 43)
+        Me.ButtonPLUSMOINS.TabIndex = 20
+        Me.ButtonPLUSMOINS.TabStop = False
+        Me.ButtonPLUSMOINS.Text = "±"
+        Me.ButtonPLUSMOINS.UseVisualStyleBackColor = False
+        '
+        'txtTOTAL
+        '
+        Me.txtTOTAL.Font = New System.Drawing.Font("Arial", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtTOTAL.Location = New System.Drawing.Point(5, 356)
+        Me.txtTOTAL.MaxLength = 10
+        Me.txtTOTAL.Multiline = True
+        Me.txtTOTAL.Name = "txtTOTAL"
+        Me.txtTOTAL.Size = New System.Drawing.Size(234, 40)
+        Me.txtTOTAL.TabIndex = 21
+        Me.txtTOTAL.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoSize = True
+        Me.BackColor = System.Drawing.SystemColors.ControlLight
+        Me.ClientSize = New System.Drawing.Size(243, 401)
+        Me.Controls.Add(Me.txtTOTAL)
+        Me.Controls.Add(Me.ButtonPLUSMOINS)
+        Me.Controls.Add(Me.ButtonC)
+        Me.Controls.Add(Me.ButtonBACK)
+        Me.Controls.Add(Me.ButtonTOTAL)
+        Me.Controls.Add(Me.ButtonSOUS)
+        Me.Controls.Add(Me.ButtonADDI)
+        Me.Controls.Add(Me.ButtonMULT)
+        Me.Controls.Add(Me.ButtonDIVI)
+        Me.Controls.Add(Me.ButtonPOINT)
+        Me.Controls.Add(Me.Button0)
+        Me.Controls.Add(Me.Button9)
+        Me.Controls.Add(Me.Button8)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.txtOperation)
+        Me.Font = New System.Drawing.Font("Arial", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.ShowIcon = False
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Calculatrice"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtOperation As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button9 As Button
+    Friend WithEvents Button0 As Button
+    Friend WithEvents ButtonPOINT As Button
+    Friend WithEvents ButtonDIVI As Button
+    Friend WithEvents ButtonMULT As Button
+    Friend WithEvents ButtonADDI As Button
+    Friend WithEvents ButtonSOUS As Button
+    Friend WithEvents ButtonTOTAL As Button
+    Friend WithEvents ButtonBACK As Button
+    Friend WithEvents ButtonC As Button
+    Friend WithEvents ButtonPLUSMOINS As Button
+    Friend WithEvents txtTOTAL As TextBox
+End Class
---
+++ Calculatrice/Form1.vb
@@ -0,0 +1,111 @@
+﻿Imports System.CodeDom.Compiler
+Imports System.Reflection
+
+Public Class Form1
+
+    Private Sub Button_Click(sender As Object, e As EventArgs) Handles Button0.Click, Button1.Click, Button2.Click, Button3.Click, Button4.Click, Button5.Click, Button6.Click, Button7.Click, Button8.Click, Button9.Click, ButtonADDI.Click, ButtonBACK.Click, ButtonC.Click, ButtonDIVI.Click, ButtonMULT.Click, ButtonPLUSMOINS.Click, ButtonPOINT.Click, ButtonSOUS.Click, ButtonTOTAL.Click
+        DisplayCaracter(sender)
+    End Sub
+
+
+    Private Sub DisplayCaracter(btn As Button)
+        Select Case btn.Name
+            Case "Button0"
+                txtOperation.Text = txtOperation.Text & "0"
+            Case "Button1"
+                txtOperation.Text = txtOperation.Text & "1"
+            Case "Button2"
+                txtOperation.Text = txtOperation.Text & "2"
+            Case "Button3"
+                txtOperation.Text = txtOperation.Text & "3"
+            Case "Button4"
+                txtOperation.Text = txtOperation.Text & "4"
+            Case "Button5"
+                txtOperation.Text = txtOperation.Text & "5"
+            Case "Button6"
+                txtOperation.Text = txtOperation.Text & "6"
+            Case "Button7"
+                txtOperation.Text = txtOperation.Text & "7"
+            Case "Button8"
+                txtOperation.Text = txtOperation.Text & "8"
+            Case "Button9"
+                txtOperation.Text = txtOperation.Text & "9"
+            Case "ButtonADDI"
+                txtOperation.Text = txtOperation.Text & "+"
+            Case "ButtonSOUS"
+                txtOperation.Text = txtOperation.Text & "-"
+            Case "ButtonDIVI"
+                txtOperation.Text = txtOperation.Text & "/"
+            Case "ButtonMULT"
+                txtOperation.Text = txtOperation.Text & "*"
+            Case "ButtonPOINT"
+                txtOperation.Text = txtOperation.Text & "."
+            Case "ButtonPLUSMOINS"
+                txtOperation.Text = "-" & txtOperation.Text
+            Case "ButtonC"
+                txtOperation.Text = ""
+                txtTOTAL.Text = ""
+                txtOperation.Focus()
+            Case "ButtonBACK"
+                If Len(txtOperation.Text) > 0 Then
+                    txtOperation.Text = Mid(txtOperation.Text, 1, Len(txtOperation.Text) - 1)
+                    txtOperation.SelectionStart = Len(txtOperation.Text)
+                    txtOperation.Focus()
+                End If
+            Case "ButtonTOTAL"
+                txtTOTAL.Text = TransformOperation(Trim(Replace(txtOperation.Text, " ", "")))
+                txtOperation.Focus()
+                txtOperation.SelectionStart = Len(txtOperation.Text)
+                '   txtOperation.Text = ""
+        End Select
+
+    End Sub
+
+    Private Sub txtOperation_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtOperation.KeyPress
+        'empeche la clé enter de changer de ligne 
+        If e.KeyChar = Chr(13) Then
+            e.KeyChar = ChrW(Keys.None)
+        End If
+    End Sub
+
+    Private Sub txtOperation_KeyUp(sender As Object, e As KeyEventArgs) Handles txtOperation.KeyUp
+        'récupère les clés du clavier
+        Select Case e.KeyCode
+            Case Keys.Enter
+                Button_Click(ButtonTOTAL, EventArgs.Empty)
+            Case Keys.Delete
+                Button_Click(ButtonC, EventArgs.Empty)
+            Case Else
+                If Len(txtOperation.Text) = 35 Then MsgBox("35 caracters maximum")
+                If Len(txtOperation.Text) = 0 Then txtTOTAL.Text = ""
+        End Select
+
+    End Sub
+
+    Private Function TransformOperation(ByVal txt As String) As String
+        Dim vbCProvider As New VBCodeProvider
+        Dim comp As New CompilerParameters 'nouvel objet compilateur
+        comp.GenerateExecutable = False
+        comp.GenerateInMemory = True
+
+        Dim TempModuleSource As String = "Imports System" & Environment.NewLine &
+             "Namespace ns " & Environment.NewLine &
+            "Public Class clComp" & Environment.NewLine &
+            "Public Shared Function Evaluate()" & Environment.NewLine &
+            "Return " & txt & Environment.NewLine &
+            "End Function" & Environment.NewLine &
+            "End Class" & Environment.NewLine &
+            "End Namespace"
+
+        'invoque la class compilateur et appelle sa fonction evaluate ou récupére son erreur
+        Dim compRes As CompilerResults = vbCProvider.CompileAssemblyFromSource(comp, TempModuleSource)
+        If compRes.Errors.Count > 0 Then
+            Return "Erreur*"
+        Else
+            Dim mInfo As MethodInfo = compRes.CompiledAssembly.GetType("ns.clComp").GetMethod("Evaluate")
+            Return Convert.ToString(mInfo.Invoke(Nothing, Nothing))
+        End If
+    End Function
+
+
+End Class
